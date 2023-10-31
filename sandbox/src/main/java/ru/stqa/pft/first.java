cc66215948eef148a81ac82ae9323d3ae811a296
Tyutyakova <p-v-a19@mail.ru>
2023-10-31 23:46:45 +0300

    Update first.java

--- sandbox/src/main/java/ru/stqa/pft/first.java
+++ sandbox/src/main/java/ru/stqa/pft/first.java
@@ -4,24 +4,18 @@ public class first {
 public static void main (String[] args) {
    hello("Anna");
    square(4);
-  System.out.println ("Gkjoflm " + area(3));
-  double a = 4;
-  double b = 5;
-  System.out.println ("Gkjoflm2 " + area(a, b));
-}
+
+   Square as = new Square(3);
+   Rectangle re = new Rectangle(4, 5);
+  System.out.println ("Rectangle " + re.area());
+  System.out.println ("Gkjoflm " + as.area() );
+ }
 public static void hello (String userName) {
     System.out.println ("Hello, " + userName + "!!!!!");
 }
 public static void square (double len) {
   double s = len * len;
   System.out.println ("Площадь " + s );
-}
-public static  double area (double len) {
-  return len * len;
-}
-public static double area (double a, double b ){
-  return a * b;
-
 }
 
 }
