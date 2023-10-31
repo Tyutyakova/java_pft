766503f5817851431e86b29354aef8f82855a34c
Tyutyakova <p-v-a19@mail.ru>
2023-10-31 23:45:22 +0300

    factorial

---
+++ sandbox/src/main/java/ru/stqa/pft/Factor.java
@@ -0,0 +1,11 @@
+package ru.stqa.pft;
+
+import static ru.stqa.pft.Area.getFactorial;
+
+public class Factor {
+  public static void main (String[] args) {
+
+    System.out.println(getFactorial(4));
+    }
+
+}
