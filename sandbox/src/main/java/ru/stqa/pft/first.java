5c377a79eacbac9ba179df7a67819451e3eaba34
Tyutyakova <p-v-a19@mail.ru>
2023-10-31 20:05:00 +0300

    добавлены функции

--- sandbox/src/main/java/ru/stqa/pft/first.java
+++ sandbox/src/main/java/ru/stqa/pft/first.java
@@ -2,6 +2,26 @@ package ru.stqa.pft;
 
 public class first {
 public static void main (String[] args) {
-System.out.println ("Hello, java!!!!!");
+   hello("Anna");
+   square(4);
+  System.out.println ("Gkjoflm " + area(3));
+  double a = 4;
+  double b = 5;
+  System.out.println ("Gkjoflm2 " + area(a, b));
 }
+public static void hello (String userName) {
+    System.out.println ("Hello, " + userName + "!!!!!");
+}
+public static void square (double len) {
+  double s = len * len;
+  System.out.println ("Площадь " + s );
+}
+public static  double area (double len) {
+  return len * len;
+}
+public static double area (double a, double b ){
+  return a * b;
+
+}
+
 }
